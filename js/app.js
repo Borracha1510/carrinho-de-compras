914d1cb0a85d5a57c8131603d36c9439561072bd
Matheus Soares Ascenso <105125628+Borracha1510@users.noreply.github.com>
2024-05-03 07:07:57 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//limpar campo carrinho de compra que aparece no iníc
+limpar();
+
+let totalGeral = 0;
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    //Verificar se a quantidade inserida é válida
+    if(quantidade == '') {
+        confirm('ATENÇÂO: Selecione a quantidade!');
+        return;
+    }
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    //adicionar no carrinho 
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+    </section>`;
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total'); 
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = '';
+    
+}
+
+function limpar() {
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
+}
